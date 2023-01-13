a93b6d34498344cf10c566dfcd83fc2f2995f748
Marina Papapetrou <contact@marinapapap.com>
2023-01-13 11:38:43 +0000

    updated e2e tests for signing_up feature and all passing

--- frontend/cypress/e2e/signing_up.cy.js
+++ frontend/cypress/e2e/signing_up.cy.js
@@ -3,6 +3,9 @@ describe("Signing up", () => {
     cy.visit("/signup");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
+    cy.get("#first-name").type("First");
+    cy.get("#last-name").type("Last");
+    cy.get("#user-dob").type("2000-10-10");
     cy.get("#submit").click();
 
     cy.url().should("include", "/login");
@@ -11,6 +14,9 @@ describe("Signing up", () => {
   it("with missing password, redirects to '/signup'", () => {
     cy.visit("/signup");
     cy.get("#email").type("someone@example.com");
+    cy.get("#first-name").type("First");
+    cy.get("#last-name").type("Last");
+    cy.get("#user-dob").type("2000-10-10");
     cy.get("#submit").click();
 
     cy.url().should("include", "/signup");
@@ -19,6 +25,42 @@ describe("Signing up", () => {
   it("with missing email, redirects to '/signup'", () => {
     cy.visit("/signup");
     cy.get("#password").type("password");
+    cy.get("#first-name").type("First");
+    cy.get("#last-name").type("Last");
+    cy.get("#user-dob").type("2000-10-10");
+    cy.get("#submit").click();
+
+    cy.url().should("include", "/signup");
+  });
+
+  it("with missing first name, redirects to '/signup'", () => {
+    cy.visit("/signup");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#last-name").type("Last");
+    cy.get("#user-dob").type("2000-10-10");
+    cy.get("#submit").click();
+
+    cy.url().should("include", "/signup");
+  });
+
+  it("with missing last name, redirects to '/signup'", () => {
+    cy.visit("/signup");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#first-name").type("First");
+    cy.get("#user-dob").type("2000-10-10");
+    cy.get("#submit").click();
+
+    cy.url().should("include", "/signup");
+  });
+
+  it("with missing user dob, redirects to '/signup'", () => {
+    cy.visit("/signup");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#first-name").type("First");
+    cy.get("#last-name").type("Last");
     cy.get("#submit").click();
 
     cy.url().should("include", "/signup");
