ca6db47eb517e6f35d0839b94203f8fb338d7223
CodeLee13 <leonpeterkin@icloud.com>
2023-01-13 15:08:15 +0000

    Making a post test

---
+++ frontend/cypress/e2e/making_a_post.cy.js
@@ -0,0 +1,17 @@
+describe("Making a post", () => {
+  before(() => {
+    cy.signup("user@email.com", "12345678")
+    cy.login("user@email.com", "12345678")
+  })
+  it("user succesfully creates a post", () => {
+    cy.visit("/posts");
+    cy.get("#postInput").type("yay");
+    cy.get("#submitPost").click();
+
+    cy.url().should("include", "/posts");
+    cy.get('[data-cy="post"]')
+    .should('contain.text', "yay")
+  });
+})
+
+// we want to the user to visit the post page, then type in the box, then click the post button. we want to see a post of the text the user inputed in the box.
