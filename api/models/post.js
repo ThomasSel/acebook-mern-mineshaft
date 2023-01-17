5306e871b04d5a07aff5ea27593b0ed8105f00e6
Marina Papapetrou <contact@marinapapap.com>
2023-01-17 16:42:48 +0000

    Passed all tests in 'Comment Schema' test function in model post spec file.

--- api/models/post.js
+++ api/models/post.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
+const commentSchema = new mongoose.Schema({ message: {type: String, required: true }})
+
 const PostSchema = new mongoose.Schema({
-  message: String
+  message: { type: String, required: true },
+  comments: [commentSchema]
 });
 
 const Post = mongoose.model("Post", PostSchema);
--- api/spec/models/post.spec.js
+++ api/spec/models/post.spec.js
@@ -37,4 +37,59 @@ describe("Post model", () => {
       });
     });
   });
+
+  describe("Comments Schema", () => {
+    it("Post has no comments", () => {
+      var post = new Post({ message: "some message" });
+
+      expect(post.comments.length).toEqual(0);
+    })
+
+    it("comment has a message", () => {
+      var post = new Post({ message: "some message" });
+      post.comments.push({message: "this is comment"});
+      expect(post.comments[0].message).toEqual("this is comment");
+    });
+
+    it("can save a post with a comment", (done) => {
+      var post = new Post({ message: "some message" });
+      post.comments.push({message: "this is comment"});
+  
+      post.save((err) => {
+        expect(err).toBeNull();
+  
+        Post.find((err, posts) => {
+          expect(err).toBeNull();
+  
+          expect(posts[0].comments[0]).toMatchObject({ message: "this is comment" });
+          done();
+        });
+      });
+    });
+
+    it("can add a comment to a saved post", (done) => {
+      var post = new Post({ message: "some message" });
+  
+      post.save((err) => {
+        expect(err).toBeNull();
+  
+        Post.findById(post._id, (err, savedPost) => {
+          expect(err).toBeNull();
+          savedPost.comments.push({message: "this is comment"});
+
+          savedPost.save((err) => {
+              expect(err).toBeNull();
+
+              Post.find((err, posts) => {
+                expect(err).toBeNull();
+        
+                expect(posts[0].comments[0]).toMatchObject({ message: "this is comment" });
+                done();
+              });
+           });
+        });
+      });
+    });
+  })
+  
 });
