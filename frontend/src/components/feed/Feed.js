f1c3deec9c36672d5e6d8650a5de82b2ee0da61b
m <32762403+m-codes@users.noreply.github.com>
2023-01-17 16:08:20 +0000

    integrate the text box in the feed with css

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,106 +1,55 @@
-import React, { useEffect, useState } from "react";
 import Post from "../post/Post";
+import PostInputForm from "../postInputForm/PostInputForm";
+import React, { useEffect, useState } from "react";
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
 
-  // new post bar and logic for testing purposes
-  const [newPost, setNewPost] = useState("");
-
-  const updatePosts = () => {
-    fetch("/posts", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((response) => response.json())
-      .then(async (data) => {
-        window.localStorage.setItem("token", data.token);
-        setToken(window.localStorage.getItem("token"));
-        console.log(data.posts)
-        setPosts(data.posts);
-      });
-  };
-
   useEffect(() => {
     if (token) {
-      updatePosts();
+      fetch("/posts", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => {
+          if (response.status === 401) {
+            window.localStorage.removeItem("token");
+            navigate("/login");
+          } else {
+            return response.json();
+          }
+        })
+        .then(async (data) => {
+          window.localStorage.setItem("token", data.token);
+          setToken(window.localStorage.getItem("token"));
+          setPosts(data.posts);
+        });
+    } else {
+      navigate("/login");
     }
   }, []);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    fetch("/posts", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({ message: newPost }),
-    })
-      .then((response) => {
-        updatePosts();
-        if (response.status === 201) {
-          // navigate('/posts')
-          console.log("yay");
-          // console.log(response.json())
-          // return response.json()
-        } else {
-          console.log("noooo");
-          // navigate('/signup')
-        }
-      })
-      .then(async (data) => {
-        console.log(data);
-      });
-  };
-
-  const handleNewPost = (event) => {
-    setNewPost(event.target.value);
-  };
 
   const logout = () => {
     window.localStorage.removeItem("token");
     navigate("/login");
   };
-  // end
 
-  if (token) {
-    return (
-      <>
-        <h2>Posts</h2>
-        <button onClick={logout}>Logout</button>
+  return (
+    <>
+      <h2>Posts</h2>
+      <button onClick={logout}>Logout</button>
 
-        {/* new post prototype for testing */}
-        <form onSubmit={handleSubmit}>
-          <input
-            placeholder="Type your post here"
-            id="newPost"
-            type="newPost"
-            value={newPost}
-            onChange={handleNewPost}
-          />
-          <input
-            role="submit-button"
-            id="submit"
-            type="submit"
-            value="Submit"
-          />
-        </form>
-        {/* end */}
+      <PostInputForm token={token} setToken={setToken} setPosts={setPosts} />
 
-        <div class="" id="feed" role="feed">
-          {posts.map((post) => (
-            <Post post={post} key={post._id} />
-          ))}
-        </div>
-      </>
-    );
-  } else {
-    navigate("/signin");
-  }
+      <div id="feed" role="feed">
+        {posts.map((post) => (
+          <Post post={post} key={post._id} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Feed;
