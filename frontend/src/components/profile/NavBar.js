119553234bc137618316500c4fdbcb1bdc4ab3bf
Magdaline <mamoakohene@gmail.com>
2023-01-18 13:56:21 +0000

    testing new functionalities

--- frontend/src/components/profile/NavBar.js
+++ frontend/src/components/profile/NavBar.js
@@ -13,13 +13,21 @@ const NavBar = (props) => {
           </a>
         </div>
         <div>
-          <button
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn m-1 bg-blue-500 hover:bg-white text-white font-bold hover:text-blue-500 py-2 px-4 rounded-full">Menu</label>
+          <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a href="/posts">Feed</a></li>
+            <li><a href="/login"
+            id="logout-button">Logout</a></li>
+          </ul>
+        </div>
+          {/* <button
             class="bg-blue-500 hover:bg-white text-white font-bold hover:text-blue-500 py-2 px-4 rounded-full"
             onClick={props.logout}
             id="logout-button"
           >
             Logout
-          </button>
+          </button> */}
         </div>
       </nav>
     </>
