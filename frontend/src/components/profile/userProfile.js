b980a7bc443f4a8baf490771ead3b7656e7f77f2
Magdaline <mamoakohene@gmail.com>
2023-01-18 11:36:52 +0000

    creating user profile page

---
+++ frontend/src/components/profile/userProfile.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+const UserProfile = ({ navigate }) => {
+  
+  return (
+    <h1>
+      profile page
+    </h1>
+  )
+}
+
+export default UserProfile;
