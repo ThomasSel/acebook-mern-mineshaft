19b59626994803540694f7d5a500a748092db728
Graeme Paterson <ghpaterson@gmail.com>
2023-01-18 15:31:33 +0000

    dropdown link and logo link to feed tested

--- frontend/cypress/e2e/user_profile.cy.js
+++ frontend/cypress/e2e/user_profile.cy.js
@@ -1,8 +1,34 @@
 describe("User profile", () => {
+  before(() => {
+    cy.signup("user@email.com", "12345678");
+  });
+
   it("logs the user out", () => {
     cy.visit("/profile");
 
     cy.get("#logout-button").click();
-    cy.url().should("include", "/login")
-  })
-})
+    cy.url().should("include", "/login");
+  });
+
+  it("redirects user to the feed page from the dropdown menu", () => {
+    cy.visit("/login");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    cy.visit("/profile");
+    cy.get("#feed-button").click();
+    cy.url().should("include", "/posts");
+  });
+
+  it("redirects user to the feed page from the logo", () => {
+    cy.visit("/login");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    cy.visit("/profile");
+    cy.get("#logo-link").click();
+    cy.url().should("include", "/posts");
+  });
+});
--- frontend/src/components/profile/NavBar.js
+++ frontend/src/components/profile/NavBar.js
@@ -8,19 +8,36 @@ const NavBar = (props) => {
           <a
             className="font-lobster text-white text-center hover:text-blue-900 text-6xl"
             href="/posts"
+            id="logo-link"
           >
             acebook
           </a>
         </div>
         <div>
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn m-1 bg-blue-500 hover:bg-white text-white font-bold hover:text-blue-500 py-2 px-4 rounded-full">Menu</label>
-          <ul id="parent" tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-            <li className="visible"><a href="/posts">Feed</a></li>
-            <li className="visible"><a onClick={props.logout}
-            id="logout-button">Logout</a></li>
-          </ul>
-        </div>
+          <div className="dropdown dropdown-end">
+            <label
+              tabIndex={0}
+              className="btn m-1 bg-blue-500 hover:bg-white text-white font-bold hover:text-blue-500 py-2 px-4 rounded-full"
+            >
+              Menu
+            </label>
+            <ul
+              id="parent"
+              tabIndex={0}
+              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li className="visible">
+                <a href="/posts" id="feed-button">
+                  Feed
+                </a>
+              </li>
+              <li className="visible">
+                <a onClick={props.logout} id="logout-button">
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
