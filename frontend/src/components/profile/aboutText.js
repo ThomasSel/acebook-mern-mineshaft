b1644066a80a143bae71984dffec93725256ba4a
Graeme Paterson <ghpaterson@gmail.com>
2023-01-19 15:51:39 +0000

    data policy h2 change

--- frontend/src/components/profile/aboutText.js
+++ frontend/src/components/profile/aboutText.js
@@ -7,7 +7,7 @@ const AboutText = (props) => {
         <div className="container mx-auto h-full flex justify-center items-center">
           <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <h2 className="font-lobster text-blue-500 text-center text-3xl mb-12">
-              About
+              Data policy
             </h2>
             <div className="text-center">
               <p>
