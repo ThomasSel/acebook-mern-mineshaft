90025c5b785e4dd2bcf50d01deccd63d943495d2
Graeme Paterson <ghpaterson@gmail.com>
2023-01-19 15:39:03 +0000

    corrected unsaved child component about text

--- frontend/src/components/profile/userProfile.js
+++ frontend/src/components/profile/userProfile.js
@@ -21,37 +21,6 @@ const UserProfile = ({ navigate }) => {
     <>
       <NavBar logout={logout} />
       <AboutText />
-      {/* <div className="bg-grey-lighter h-screen font-sans">
-        <div className="container mx-auto h-full flex justify-center items-center">
-          <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <h2 className="font-lobster text-blue-500 text-center text-3xl mb-12">
-              About
-            </h2>
-            <div className="text-center">
-              <p>
-                Acebook stores your information on every app and extention you
-                use.
-              </p>
-              <p>
-                We know how often you use them, where you use them, and who you
-                use them to interact with.
-              </p>
-              <p>
-                That means we know who you talk to on Instagram, what countries
-                you are speaking with,
-              </p>
-              <p>
-                your relationship status, probably weight, if you're going to be
-                a parent soon
-              </p>
-              <p>and what time you go to sleep!</p>
-              <p>Sleep tight xoxo</p>
-              <br></br>
-              <p>- The team at Acebook.</p>
-            </div>
-          </form>
-        </div>
-      </div> */}
     </>
   );
 };
