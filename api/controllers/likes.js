7414dfe9d938a4ea22dda00dcd93b0f1ee32b714
Daisy <ysiad926@gmail.com>
2023-01-20 11:35:48 +0000

    render the like numbers

--- api/controllers/likes.js
+++ api/controllers/likes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Like = require("../models/like")
 const TokenGenerator = require("../models/token_generator")
 // 
+
 const LikeController = {
   Index: (req,res) => {
     let variable = req.params.postId;
@@ -9,7 +10,7 @@ const LikeController = {
     .exec((err, likes) => {
       const token = TokenGenerator.jsonwebtoken(req.user_id);
       if(err) return res.status(400).send(err);
-      res.status(200).json({ success: true, likes, token: token});
+      res.status(200).json({ success: true, likes: likes, token: token});
     });
   },
   Create: (req,res) => {
@@ -17,17 +18,20 @@ const LikeController = {
       postId: req.body.postId,
       userId: req.user_id
     };
+
     const like = new Like(likeData);
     let variable = req.body.postId;
     const token = TokenGenerator.jsonwebtoken(req.user_id);
+ 
     like.save(saveError => {
       if (saveError) return res.status(400).send(JSON.stringify({saveError: saveError}));
 
       Like.find({postId: variable})
         .exec((err, likes) => {
           if(err) return res.status(400).send(JSON.stringify({findError: err}));
-          res.status(200).json({ success: true, likes: likes, token: token})
+          res.status(200).json({ success: true, likes: likes, token: token});
         })
+        
     });
   },
 };
--- frontend/src/components/like/Like.js
+++ frontend/src/components/like/Like.js
@@ -1,18 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import Axios from 'axios';
+// import { ReactDOM } from "react";
+
+// const root = ReactDOM.createRoot(document.getElementById('like-button'))
+// // root.render()
 
 function Like(props){
   const [Likes, setLikes] = useState(0)
   const [LikeAction, setLikeAction] = useState(null)
   const [token, setToken] = useState(window.localStorage.getItem("token"));
-  console.log(token)
+  const [UserArray, setUserArray] = useState([]);
+
+  function parseJwt(token) {
+    if (!token) { return; }
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64));
+}
+const props_userId = parseJwt(token).user_id
 
   useEffect(() => {
-  
-    // let variable = { postId: props.postId, userId: props.userId};
-
-    
-      Axios.get(`/likes/${props.postId}`, {
+    Axios.get(`/likes/${props.postId}`, {
         headers: {
           'Authorization': `Bearer ${token}`
         }
@@ -23,18 +31,18 @@ function Like(props){
         setLikes(response.data.likes.length)
         //if I already click this like button or not
         response.data.likes.map(like => {
-          console.log(`like: ${like.postId}`)
-          console.log(`props: ${props.postId}`)
-          if(like.userId === props.userId){
-            console.log(`like.userId:${like.userId}`)
-            console.log(`props.postId: ${props.postId}`)
-            console.log(`props.userId: ${props.userId}`)
+          if(like.userId === props_userId){
             setLikeAction('liked')
-          }else{
-            setLikeAction(null)
           }
         })
-        console.log(Likes);
+        const newArray = []
+        response.data.likes.map(like => {
+          console.log(like)
+          newArray.push(like.userId)
+        })
+        setUserArray(newArray)
+        console.log(UserArray)
+
       }else {
         alert('Failed to get likes')
       }
@@ -43,55 +51,41 @@ function Like(props){
   },[])
 
   const handleClick = (event) => {
-    //Axios.post
-    if(token){
     event.preventDefault();
-    console.log(JSON.stringify({ postId: props.postId}))
+    // window.location.reload(true)
+    //Axios.post
+    if(token) {
 
-    fetch('/likes',{
-      method: 'post',
-      headers: { 
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`},
+      fetch("/likes", {
+        method: "post",
+        headers: { 
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
         body: JSON.stringify({ postId: props.postId})
-    })
-    .then((response) => {
-      console.log(`response.json: ${response.json()}`)
-      return response.json()
-      
-    })
-    
-    .catch(err => console.error(err))
-
-    .then(async (data) => {
-      console.log(`data: ${data}`)
-      window.localStorage.setItem("token", data.token);
-      setToken(window.localStorage.getItem("token"));
-      setLikes(data.likes.length);
-      console.log(`likes: ${data.likes.length}`)
-    })
-    .then(response => {
-      if(response.data.success) {
-        console.log(`response.data: ${response.data}`)
-        //How many likes does this post have
-        setLikes(response.data.likes.length)
-        //if I already click this like button or not
-        response.data.likes.map(like => {
-          if(like.postId === props.postId){
+      }
+      )
+      .then((response) =>{ 
+        return response.json()
+      })
+      .then(async(data) => {
+        setLikes(data.likes.length)
+        data.likes.map(like => {
+          if(like.userId === props_userId){
             setLikeAction('liked')
           }
         })
-        console.log(Likes);
-      }else {
-        alert('Failed to get likes')
-      }
+      })
+      
+      .catch(err => console.error(err))
     }
-    );
-  }
   }
 
+  
   return (
+   
     <div id="like-button" class="flex py-2 text-center">
+     <Fragment >
     <button
       // href="#"
       onClick={handleClick}
@@ -110,7 +104,9 @@ function Like(props){
       </svg>
     </button>
     <span>{Likes}</span>
+    </Fragment>
     </div>
+    
   )
 }
 
